1472f9207b1bf1b6c21e232a90e93aeda1fb67bb
Mike McDermott <mmcdermott@microsoft.com>
2024-05-09 11:45:12 -0500

    Wired up App Insights sdK for Open Telemetry

--- function_app.py
+++ function_app.py
@@ -5,7 +5,14 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from openai import AzureOpenAI
 
-# Azure OpenAI setup
+# Azure Application Insights setup
+from azure.monitor.opentelemetry import configure_azure_monitor
+from opentelemetry import trace
+from opentelemetry.propagate import extract
+
+configure_azure_monitor() # Be sure that APPINSIGHTS_INSTRUMENTATIONKEY is configured in environment variables or this call will fail
+
+# Azure OpenAI setup.  Be sure that AZURE_OPENAI_API_KEY is configured in environment variables or this call will fail
 deployment_id = os.environ["AZURE_OPENAI_DEPLOYMENT_ID"]
 aoaiClient = AzureOpenAI(
     api_version = "2024-02-15-preview",
@@ -13,92 +20,102 @@ aoaiClient = AzureOpenAI(
     azure_deployment = deployment_id
 )
 
+# Create a Kev Vault Secrets client
 kvSecretClient = SecretClient(vault_url=os.environ["KEY_VAULT_URI"], credential=DefaultAzureCredential())
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="chat")
-async def chat(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        logging.info('Python HTTP trigger function processed a request.')
-        
-        # Azure AI Search setup
-        search_endpoint = os.environ["AZURE_AI_SEARCH_URI"]; # Add your Azure AI Search endpoint here
-        search_index_name = os.environ["AZURE_AI_SEARCH_INDEX_NAME"]; # Add your Azure AI Search index name here
+async def chat(req: func.HttpRequest, context) -> func.HttpResponse:
+    
+    # Extract the trace context from the incoming request
+    carrier = {
+      "traceparent": context.trace_context.Traceparent,
+      "tracestate": context.trace_context.Tracestate,
+    }
+    
+    # Create a new Open Telemetry span with the incoming trace context
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("chat", context=extract(carrier)): # span name matches the function name
+        try:
+            logging.info('Chat API called')
+            
+            # Azure AI Search setup
+            search_endpoint = os.environ["AZURE_AI_SEARCH_URI"]
+            search_index_name = os.environ["AZURE_AI_SEARCH_INDEX_NAME"]
 
-        question = req.params.get('question')
-        if not question:
-            try:
-                req_body = req.get_json()
-            except ValueError:
-                pass
-            else:
-                question = req_body.get('question')
-        
-        messages = [
-                {
-                    "role": "user",
-                    "content": question
+            question = req.params.get('question')
+            if not question:
+                try:
+                    req_body = req.get_json()
+                except ValueError:
+                    pass
+                else:
+                    question = req_body.get('question')
+                    
+            if question:
+            
+                messages = [
+                        {
+                            "role": "system",
+                            "content": "Hello! I'm an AI assistant that helps people find information. How can I help you today?"
+                        },
+                        {
+                            "role": "user",
+                            "content": question
+                        }
+                    ]
+                
+                azure_search_api_key = await getKeyVaultSecret("azure-search-api-key")
+                
+                azure_search_data_source = {
+                    "type": "azure_search",
+                    "parameters": {
+                        "endpoint": search_endpoint,
+                        "authentication": {
+                            "type": "api_key",
+                            "key": azure_search_api_key
+                        },
+                        "index_name": search_index_name,
+                        "fields_mapping": {},
+                        "in_scope": True,
+                        "top_n_documents": 10, #TODO: configurize this
+                        "query_type": "simple",
+                        "role_information": "You are an AI assistant that helps people find information.",
+                        "filter": None
+                    }
                 }
-            ]
-        
-        azure_search_api_key = await getKeyVaultSecret("azure-search-api-key")
-        
-        azure_search_data_source = {
-            "type": "azure_search",
-            "parameters": {
-                "endpoint": search_endpoint,
-                "authentication": {
-                    "type": "api_key",
-                    "key": azure_search_api_key
-                },
-                "index_name": search_index_name,
-                "fields_mapping": {},
-                "in_scope": True,
-                "top_n_documents": 10, #TODO: configurize this
-                "query_type": "simple",
-                "role_information": "You are an AI assistant that helps people find information.",
-                "filter": None
-            }
-        }
-        
-        model_args = {
-            "messages": messages,
-            "model": deployment_id, #this is the name of the deployment from Azure AI Studio
-            "extra_body": {
-                "data_sources": [
-                    azure_search_data_source
-                ]
-            }
-        }
-        
-        completion = aoaiClient.chat.completions.create(**model_args)
-        
-        if(len(completion.choices) > 0):
-            answer = completion.choices[0].message.content
-        else:
-            answer = "no answer"
+                
+                model_args = {
+                    "messages": messages,
+                    "model": deployment_id, #this is the name of the deployment from Azure AI Studio
+                    "extra_body": {
+                        "data_sources": [
+                            azure_search_data_source
+                        ]
+                    }
+                }
+                
+                completion = aoaiClient.chat.completions.create(**model_args)
+                
+                if(len(completion.choices) > 0):
+                    answer = completion.choices[0].message.content
+                else:
+                    answer = "no answer"
 
-        if question:
-            #try:
                 return func.HttpResponse(
-                    f"Hello, {question}. This HTTP triggered function executed successfully. Answer: {answer}", 
+                    f"Question Submitted: {question}. Answer: {answer}", 
                     status_code=200
                 )
-            #except Exception as e:
-            #    return func.HttpResponse(
-            #        f"Exception: {e}", 
-            #        status_code=200
-            #    )
-                
-        else:
-            return func.HttpResponse(
-                "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-                status_code=200
-            )
         
-    except Exception as e:
-        return func.HttpResponse(f"Exception: {e}", status_code=200)
+            else:
+                return func.HttpResponse(
+                    "Please specify a question prompt in the 'question' query string parameter or in the body of the request.",
+                    status_code=200
+                )
+            
+        except Exception as e:
+            return func.HttpResponse(f"Exception: {e}", status_code=200)
         
     
 async def getKeyVaultSecret(secretName: str) -> str:
